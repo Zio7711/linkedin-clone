8e1f11367bcfe3effe9c5a150a9abd374b50aba2
Zio <zi.ao.tan@outlook.com>
2021-09-22 16:52:20 +0800

    style the header

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,12 +1,17 @@
 import React from 'react';
 import './Header.css';
+import SearchIcon from '@material-ui/icons/Search';
 
 const Header = () => {
   return (
     <div className='header'>
       <div className='header_left'>
-        <img src='' alt='' />
+        <img
+          src='https://img0.baidu.com/it/u=1628971908,1325531955&fm=26&fmt=auto'
+          alt=''
+        />
         <div className='header_search'>
+          <SearchIcon />
           <input type='text' />
         </div>
       </div>
