9087c3c8b452d5686f1b735cd1a4d0c2a2c83677
Zio <zi.ao.tan@outlook.com>
2021-09-24 11:59:49 +0800

    style the widgets component

--- src/components/Widgets.js
+++ src/components/Widgets.js
@@ -1,8 +1,33 @@
 import React from 'react';
 import './Widgets.css';
+import InfoIcon from '@material-ui/icons/Info';
+import { FiberManualRecord } from '@material-ui/icons';
 
-function Widgets() {
-  return <div className='widgets'></div>;
-}
+const Widgets = () => {
+  const newsArticle = (heading, subtitle) => (
+    <div className='widgets_article'>
+      <div className='widgets_articleLeft'>
+        <FiberManualRecord />
+      </div>
+      <div className='widgets_articleRight'>
+        <h4>{heading}</h4>
+        <p>{subtitle}</p>
+      </div>
+    </div>
+  );
+  return (
+    <div className='widgets'>
+      <div className='widgets_header'>
+        <h2>LinkedIn News</h2>
+        <InfoIcon />
+      </div>
+      {newsArticle('Papa Recent is back', 'Top News')}
+      {newsArticle('Papa Recent is back', 'Top News')}
+      {newsArticle('Papa Recent is back', 'Top News')}
+      {newsArticle('Papa Recent is back', 'Top News')}
+      {newsArticle('Papa Recent is back', 'Top News')}
+    </div>
+  );
+};
 
 export default Widgets;
