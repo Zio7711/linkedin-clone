0590eb7f53b7af715ef38d74b67eac859d083248
Zio <zi.ao.tan@outlook.com>
2021-09-24 11:05:37 +0800

    finish register flow

--- src/components/Header.js
+++ src/components/Header.js
@@ -7,8 +7,18 @@ import SupervisorAccountIcon from '@material-ui/icons/SupervisorAccount';
 import BusinessCenterIcon from '@material-ui/icons/BusinessCenter';
 import ChatIcon from '@material-ui/icons/Chat';
 import NotificationsIcon from '@material-ui/icons/Notifications';
+import { useDispatch } from 'react-redux';
+import { auth } from '../firebase';
+import { logout } from '../features/userSlice';
 
 const Header = () => {
+  const dispatch = useDispatch();
+
+  const logoutOfApp = () => {
+    dispatch(logout());
+    auth.signOut();
+  };
+
   return (
     <div className='header'>
       <div className='header_left'>
@@ -30,6 +40,7 @@ const Header = () => {
         <HeaderOption
           avatar='http://compassionate-leakey-e9b16b.netlify.app/images/IG_Sonny.jpeg'
           title='me'
+          onClick={logoutOfApp}
         />
       </div>
     </div>
