f0d391bbaf66a220b82208351f714333da7282b1
Zio <zi.ao.tan@outlook.com>
2021-09-24 10:11:05 +0800

    config redux for user

--- src/App.js
+++ src/App.js
@@ -1,16 +1,26 @@
+import { useSelector } from 'react-redux';
 import './App.css';
 import Feed from './components/Feed';
 import Header from './components/Header';
+import Login from './components/Login';
 import Sidebar from './components/Sidebar';
+import { selectUser } from './features/userSlice';
 
 function App() {
+  const user = useSelector(selectUser);
+
   return (
     <div className='app'>
       <Header />
-      <div className='app_body'>
-        <Sidebar />
-        <Feed />
-      </div>
+
+      {user ? (
+        <Login />
+      ) : (
+        <div className='app_body'>
+          <Sidebar />
+          <Feed />
+        </div>
+      )}
     </div>
   );
 }
