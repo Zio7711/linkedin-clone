5e6569ea00d4b0f0c2913b4d7672c311167f6082
Zio <zi.ao.tan@outlook.com>
2021-09-24 11:41:50 +0800

    add widgets component

--- src/App.js
+++ src/App.js
@@ -5,6 +5,7 @@ import Feed from './components/Feed';
 import Header from './components/Header';
 import Login from './components/Login';
 import Sidebar from './components/Sidebar';
+import Widgets from './components/Widgets';
 import { login, logout, selectUser } from './features/userSlice';
 import { auth } from './firebase';
 
@@ -39,6 +40,7 @@ function App() {
         <div className='app_body'>
           <Sidebar />
           <Feed />
+          <Widgets />
         </div>
       )}
     </div>
